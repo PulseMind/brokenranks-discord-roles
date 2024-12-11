ce1fa01fe9855338b9455b7a6b9b30e8c8b707d9
PulseMind <contact@pulsemind.pl>
2024-12-11 20:59:12 +0100

    Add and configure: LogBack, DotEnv, Discord JDA

--- discord-bot/build.gradle.kts
+++ discord-bot/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
+    implementation("net.dv8tion:JDA:5.2.1");
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.test {
--- discord-bot/src/main/java/pl/pulsemind/Main.java
+++ discord-bot/src/main/java/pl/pulsemind/Main.java
@@ -1,8 +1,13 @@
 package pl.pulsemind;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Dotenv dotenv = Dotenv.load();
+        JDABuilder builder = JDABuilder.createDefault(dotenv.get("DISCORD_BOT_TOKEN"));
+        builder.build();
     }
 }
