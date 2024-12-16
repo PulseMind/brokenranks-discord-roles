8c3a0bdd0b6ffbb8823d521ab4982f7b597c08db
PulseMind <contact@pulsemind.pl>
2024-12-16 11:29:57 +0100

    Add RoleUtil

---
+++ discord-bot/src/main/java/pl/pulsemind/utils/RoleUtil.java
@@ -0,0 +1,37 @@
+package pl.pulsemind.utils;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Icon;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.requests.restaction.RoleAction;
+import org.jetbrains.annotations.Nullable;
+import pl.pulsemind.Main;
+
+import java.awt.*;
+
+@UtilityClass
+public class RoleUtil {
+
+    public void createRole(@NonNull String guildId, @NonNull String name, @Nullable Color color, @Nullable Icon icon) {
+        Guild guild = Main.instance().getJda().getGuildById(guildId);
+        if(guild == null) return;
+
+        RoleAction roleAction = guild.createRole();
+        roleAction.setName(name);
+        if(color != null) roleAction.setColor(color);
+        if(icon != null) roleAction.setIcon(icon);
+
+        roleAction.queue();
+    }
+
+    public Role getRole(@NonNull String guildId, @NonNull String name) {
+        Guild guild = Main.instance().getJda().getGuildById(guildId);
+        if(guild == null) return null;
+        return guild.getRoles().stream()
+                .filter(role -> role.getName().equals(name))
+                .findAny()
+                .orElse(null);
+    }
+}
